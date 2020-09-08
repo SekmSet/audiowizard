9b91c5f3ba6da7dba2f5331e3953b8e6553c4274
Priscilla Joly <priscilla.joly@epitech.eu>
2020-09-08 10:33:47 +0200

    Update app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,37 +1,27 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+    BrowserRouter as Router,
 } from "react-router-dom";
 import './App.css';
 
 import Header from './components/Header';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
-import Account from './components/Account';
-import Home from './components/Home';
+
+import {AppProvider} from "./context/context";
 
 function App() {
-  return (
-      <Router>
-        <div className="App">
-            <Header />
-            <Navbar />
-            <Main />
-
-          <Switch>
-            <Route path='/' exact>
-                <Home />
-            </Route>
-
-            <Route path='/account'>
-                <Account />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-  );
+    return (
+        <AppProvider>
+            <Router>
+                <div className="App">
+                    <Header />
+                    <Navbar />
+                    <Main />
+                </div>
+            </Router>
+        </AppProvider>
+    );
 }
 
 export default App;
