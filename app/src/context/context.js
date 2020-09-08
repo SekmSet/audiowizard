840ef7c0b023ba9004d0ee6ffbc29479cbb75d9e
Priscilla Joly <priscilla.joly@epitech.eu>
2020-09-08 10:34:05 +0200

    add  - update context

--- app/src/context/context.js
+++ app/src/context/context.js
@@ -1,5 +1,32 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 
-const UserContext = React.createContext(undefined);
+const AppContext = React.createContext(undefined);
 
+export const AppProvider = ({children}) => {
+
+    const [lastName, setLastName] = useState('');
+    const [firstName, setFirstNamele] = useState('');
+    const [gender, setGender] = useState('');
+
+    const setFullName = ({last_name, first_name}) =>{
+        setLastName(last_name);
+        setFirstNamele(first_name);
+    }
+
+    const data = {
+        lastName,
+        firstName,
+        gender,
+
+        setGender,
+        setFullName,
+    }
+
+    return (
+        <AppContext.Provider value={data}>
+            {children}
+        </AppContext.Provider>
+    );
+}
+
+export default AppContext;
