51151a593192fc0ba13a77d32f4bc9cc830daacf
Priscilla Joly <priscilla.joly@epitech.eu>
2020-09-08 10:33:27 +0200

    add and updates components

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import {Route, Switch} from "react-router-dom";
+import Home from "./Home";
+import Account from "./Account";
+import Test from "./Test";
+import Results from "./Results";
 
 function Main() {
-
     return (
         <div>
+            <Switch>
+                <Route path='/' exact>
+                    <Home />
+                </Route>
 
+                <Route path='/account'>
+                    <Account />
+                </Route>
+
+                <Route path='/test'>
+                    <Test />
+                </Route>
+
+                <Route path='/results'>
+                    <Results />
+                </Route>
+            </Switch>
         </div>
     )
 }
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 function Navbar() {
     return (
         <div>
-
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/test">Commencez votre test</Link></li>
+                <li><Link to="/account">Compte</Link></li>
+            </ul>
         </div>
     )
 }
